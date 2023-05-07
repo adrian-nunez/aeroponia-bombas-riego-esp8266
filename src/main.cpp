aecd3f4d03fdacc0e63ff7a57cce94688b40f847
adrian-nunez <correo@adriannunez.com>
2023-05-07 10:44:32 -0500

    Versión con el riego de cajas (tomates)

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,26 @@
 WiFiClient wifiClient;
 HTTPClient http;
 
+const String serverName = "xxxxxxxxxxxxxxx";
 
-const String serverName = "************e/aeroponia/api/";
-
-
-const char *ssid = "aeroponia";
-const char *password = "*******";
+const char *ssid = "xxxxxxxxxxxxx";
+const char *password = "xxxxxxxxxxxx";
 
 unsigned long tiempoAnterior;
+unsigned long tiempoAnteriorTomates;
 unsigned long tiempoAnteriorApi;
 int actual;
 int cuenta;
 bool descanso;
+bool descansoTomates;
 bool cambioPines = false;
-const int pinesDisponibles[] = {4,5,12,13,14};
-int pinesHabilitados[] = {0, 1, 2, 3, 4};
+// const int pinesDisponibles[] = {4, 5, 12, 13, 14};
+// int pinesHabilitados[] = {0, 1, 2, 3, 4};
+const int pinesDisponibles[] = {4, 5, 12, 13};
+int pinesHabilitados[] = {0, 1, 2, 3};
+
+const int pinTomates = 14; // D5
+
 String pinesStringPost;
 bool arranque;
 bool pausa = false;
@@ -31,16 +36,22 @@ bool pausa = false;
 // Variables de riego
 unsigned long tiempoRiego;
 unsigned long tiempoDescanso;
+unsigned long tiempoRiegoTomates;
+unsigned long tiempoDescansoTomates;
 unsigned long tiempoApi;
 
 void encender(int pin);
 void apagar(int pin);
 void descansar();
+
 void iniciarPines();
 void CallApi();
 void enviarPost(int val, int tipo);
 void enviarPostTorres();
 void apagarPines();
+void encenderTomates();
+void apagarTomates();
+void descansarTomates();
 
 void setup()
 {
@@ -50,6 +61,10 @@ void setup()
   arranque = true;
   tiempoRiego = 30000;
   tiempoDescanso = 600000;
+
+  tiempoRiegoTomates = 45 * 1000;
+  tiempoDescansoTomates = 1000 * 60 * 5; // Va a regar los tomates cada 5 minutos
+
   tiempoApi = 8000;
 
   // Wifi
@@ -67,12 +82,14 @@ void setup()
   pinesStringPost = "";
 
   tiempoAnterior = millis();
+  tiempoAnteriorTomates = millis();
   tiempoAnteriorApi = millis();
   Serial.begin(9600);
   actual = 0;
   descanso = false;
+  descansoTomates = false;
   encender(actual);
-  
+  encenderTomates();
 }
 
 void loop()
@@ -117,6 +134,24 @@ void loop()
     encender(actual);
   }
 
+  // TOMATES
+
+  if (millis() - tiempoAnteriorTomates >= tiempoDescansoTomates + tiempoRiegoTomates && descansoTomates)
+  {
+    tiempoAnteriorTomates = millis();
+    descansoTomates = false;
+    encenderTomates();
+    Serial.println("*************** Encender pin 14");
+  }
+
+  if (!descansoTomates && (millis() - tiempoAnteriorTomates) >= tiempoRiegoTomates)
+  {
+    apagarTomates();
+    Serial.println("*************** Apagar pin 14");
+    descansoTomates = true;
+    tiempoAnteriorTomates = millis();
+  }
+
   // API
 
   if (millis() - tiempoAnteriorApi >= tiempoApi)
@@ -132,6 +167,19 @@ void encender(int pin)
   Serial.println("rele " + String(pinesDisponibles[pinesHabilitados[pin]]));
   digitalWrite(pinesDisponibles[pinesHabilitados[pin]], LOW);
   enviarPost(pinesDisponibles[pinesHabilitados[pin]], 1);
+  Serial.print("Enciende pin ");
+  Serial.println(pinesDisponibles[pinesHabilitados[pin]]);
+}
+
+void encenderTomates()
+{
+  digitalWrite(pinTomates, LOW);
+  enviarPost(pinTomates, 5);
+}
+
+void apagarTomates()
+{
+  digitalWrite(pinTomates, HIGH);
 }
 
 void apagar(int pin)
@@ -146,7 +194,11 @@ void descansar()
   enviarPost(0, 1);
 }
 
-
+void descansarTomates()
+{
+  Serial.println("Descanso Tomates");
+  enviarPost(0, 5); // tipo 5 es tomates y 0 significa descanso
+}
 
 void CallApi()
 {
@@ -159,7 +211,7 @@ void CallApi()
   else
   {
     http.begin(wifiClient, serverName.c_str());
-   
+
     int httpResponseCode = http.GET();
     if (httpResponseCode == 200)
     {
@@ -180,6 +232,7 @@ void CallApi()
           tiempoAnteriorApi = millis();
         }
 
+        // Fresas
         if ((int)tiempoRiego != (int)jsonBuffer["tiempo"])
         {
           tiempoRiego = (int)jsonBuffer["tiempo"]; // En milisegundos
@@ -192,7 +245,21 @@ void CallApi()
           tiempoAnterior = millis();
         }
 
+        // Tomates
+        if ((int)tiempoRiegoTomates != (int)jsonBuffer["tiempoT"])
+        {
+          tiempoRiegoTomates = (int)jsonBuffer["tiempoT"]; // En milisegundos
+          tiempoAnteriorTomates = millis();
+        }
+
+        if ((int)tiempoDescansoTomates != (int)jsonBuffer["descT"])
+        {
+          tiempoDescansoTomates = (int)jsonBuffer["descT"]; // En milisegundos
+          tiempoAnteriorTomates = millis();
+        }
+
         Serial.println("** Tiempos json " + String((int)jsonBuffer["tiempo"]) + " - " + String((int)jsonBuffer["desc"]));
+        Serial.println("** Tiempos json T " + String((int)jsonBuffer["tiempoT"]) + " - " + String((int)jsonBuffer["descT"]));
         // verificar si hay pausa
         if ((int)jsonBuffer["pausa"] == 1)
         {
@@ -204,6 +271,8 @@ void CallApi()
         }
 
         // Verificar si hay cambio de pines
+
+        // FALTA IMPLEMENTAR EL CAMBIO DE PINES Y PAUSA PARA LOS TOMATES
         int cuentaJson = jsonBuffer["count"];
         cambioPines = false;
         if (cuenta != cuentaJson && cuentaJson > 0)
@@ -309,7 +378,28 @@ void enviarPost(int val, int tipo)
     // int cod_respuesta = http.POST(data);
   }
 
-  
+  // tomates
+
+  if (tipo == 5)
+  {
+    data = String("rele=") + val;
+    http.begin(wifiClient, serverName + "save_tomates.php");
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    int cod_respuesta = http.POST(data);
+
+    // Websocket:
+    // (val == 0) ? descansoSocket = true : descansoSocket = false;
+
+    if (cod_respuesta > 0)
+    {
+      String cuerpo_respuesta = http.getString();
+      if (cod_respuesta != 200)
+      {
+        Serial.print("El serv respondió con error ");
+        Serial.println(cuerpo_respuesta);
+      }
+    }
+  }
 }
 
 void enviarPostTorres()
@@ -333,17 +423,28 @@ void enviarPostTorres()
 
 void iniciarPines()
 {
-  for (int p=0; p<5; p++)
+  for (int p = 0; p < 4; p++)
   {
     pinMode(pinesDisponibles[p], OUTPUT);
     digitalWrite(pinesDisponibles[p], HIGH);
+  }
+
+  if (pinTomates > 0)
+  {
+    pinMode(pinTomates, OUTPUT);
+    digitalWrite(pinTomates, HIGH);
   }
 }
 
 void apagarPines()
 {
-  for (int p=0; p<5; p++)
+  for (int p = 0; p < 5; p++)
   {
-    digitalWrite(p,HIGH);
+    digitalWrite(p, HIGH);
   }
+}
+
+void apagarPinesTomates()
+{
+  digitalWrite(pinTomates, HIGH);
 }
